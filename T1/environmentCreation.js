1a48a964f1a87b40be8236d4cabf599353d35a42
Pedro <pedroalmeidavrb@gmail.com>
2022-10-12 19:53:07 -0300

    Add cubo de forma manual

--- T1/environmentCreation.js
+++ T1/environmentCreation.js
@@ -27,8 +27,6 @@ window.addEventListener('resize', function () { onWindowResize(camera, renderer)
 // Use to scale the cube
 var scale = 1.0;
 
-// Show text information onscreen
-showInformation();
 
 // To use the keyboard
 var keyboard = new KeyboardState();
@@ -51,6 +49,8 @@ var cube = new THREE.Mesh(cubeGeometry, material);
 cube.position.set(0.0, 0.5, 0.0);
 // add the cube to the scene
 scene.add(cube);
+
+
 
 insertCube();
 
@@ -117,17 +117,6 @@ function updatePositionMessage() {
 }
 
 
-function showInformation() {
-    // Use this to show information onscreen
-    var controls = new InfoBox();
-    controls.add("Geometric Transformation");
-    controls.addParagraph();
-    controls.add("Use keyboard arrows to move the cube in XY.");
-    controls.add("Press Page Up or Page down to move the cube over the Z axis");
-    controls.add("Press 'A' and 'D' to rotate.");
-    controls.add("Press 'W' and 'S' to change scale");
-    controls.show();
-}
 
 function insertCube() {
     var cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
@@ -136,6 +125,91 @@ function insertCube() {
     cube.position.set(6.0, 0.5, 0.0);
     // add the cube to the scene
     scene.add(cube);
+
+    var cube2 = new THREE.Mesh(cubeGeometry, material);
+    cube2.position.set(8.0, 0.5, 0.0);
+    scene.add(cube2);
+
+    var cube3 = new THREE.Mesh(cubeGeometry, material);
+    cube3.position.set(7.0, 0.5, 0.0);
+    scene.add(cube3);
+
+    var cube4 = new THREE.Mesh(cubeGeometry, material);
+    cube4.position.set(0.0, 0.5, -12.0);
+    scene.add(cube4);
+
+    var cube5 = new THREE.Mesh(cubeGeometry, material);
+    cube5.position.set(-8.0, 0.5, -12.0);
+    scene.add(cube5);
+
+    var cube6 = new THREE.Mesh(cubeGeometry, material);
+    cube6.position.set(8.0, 0.5, 12.0);
+    scene.add(cube6);
+
+    var cube7 = new THREE.Mesh(cubeGeometry, material);
+    cube7.position.set(22.0, 0.5, 15.0);
+    scene.add(cube7);
+
+    var cube8 = new THREE.Mesh(cubeGeometry, material);
+    cube8.position.set(-22.0, 0.5, -4.0);
+    scene.add(cube8);
+
+    var cube9 = new THREE.Mesh(cubeGeometry, material);
+    cube9.position.set(-15.0, 0.5, -23.0);
+    scene.add(cube9);
+
+    var cube10 = new THREE.Mesh(cubeGeometry, material);
+    cube10.position.set(23.0, 0.5, 18.0);
+    scene.add(cube10);
+
+    var cube11 = new THREE.Mesh(cubeGeometry, material);
+    cube11.position.set(16.0, 0.5, 16.0);
+    scene.add(cube11);
+
+    var cube12 = new THREE.Mesh(cubeGeometry, material);
+    cube12.position.set(-16.0, 0.5, 19.0);
+    scene.add(cube12);
+
+    var cube13 = new THREE.Mesh(cubeGeometry, material);
+    cube13.position.set(17.0, 0.5, -19.0);
+    scene.add(cube13);
+
+    var cube14 = new THREE.Mesh(cubeGeometry, material);
+    cube14.position.set(20.0, 0.5, -19.0);
+    scene.add(cube14);
+
+    var cube15 = new THREE.Mesh(cubeGeometry, material);
+    cube15.position.set(-20.0, 0.5, 19.0);
+    scene.add(cube15);
+
+    var cube16 = new THREE.Mesh(cubeGeometry, material);
+    cube16.position.set(0.0, 0.5, 6.0);
+    scene.add(cube16);
+
+    var cube16 = new THREE.Mesh(cubeGeometry, material);
+    cube16.position.set(-9.0, 0.5, 6.0);
+    scene.add(cube16);
+
+    var cube17 = new THREE.Mesh(cubeGeometry, material);
+    cube17.position.set(13.0, 0.5, 7.0);
+    scene.add(cube17);
+
+    var cube18 = new THREE.Mesh(cubeGeometry, material);
+    cube18.position.set(-14.0, 0.5, -22.0);
+    scene.add(cube18);
+
+    var cube19 = new THREE.Mesh(cubeGeometry, material);
+    cube19.position.set(21.0, 0.5, 0.0);
+    scene.add(cube19);
+
+    var cube20 = new THREE.Mesh(cubeGeometry, material);
+    cube20.position.set(0.0, 0.5, 19.0);
+    scene.add(cube20);
+
+    var cube21 = new THREE.Mesh(cubeGeometry, material);
+    cube21.position.set(-5.0, 0.5, -9.0);
+    scene.add(cube21);
+
 
 }
 
