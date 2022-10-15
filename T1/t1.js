9fcf40390718ee62303effadd9250c41d74845b6
Pedro <pedroalmeidavrb@gmail.com>
2022-10-15 10:25:45 -0300

    Finalizando os detalhes

--- T1/t1.js
+++ T1/t1.js
@@ -10,6 +10,7 @@ import {
   onWindowResize,
   initCamera,
 } from "../libs/util/util.js";
+import { Vector3 } from "../build/three.module.js";
 
 let scene, renderer, light, camera, keyboard, material, clock;
 scene = new THREE.Scene(); // Create main scene
@@ -18,7 +19,7 @@ clock = new THREE.Clock();
 renderer = initRenderer(); // View function in util/utils
 light = initDefaultSpotlight(scene, new THREE.Vector3(25.0, 95.0, 50.0)); // Use default light
 material = setDefaultMaterial("#8B4513"); // create a basic material
-const borderCubeMaterial = setDefaultMaterial("#DEB887");
+const cubeMaterial = setDefaultMaterial("#DEB887");
 camera = initCamera(new THREE.Vector3(0, 20, 20)); // Init camera in this position
 
 var mixer = new Array();
@@ -37,7 +38,7 @@ keyboard = new KeyboardState();
 // cria plano
 const planeMaxSize = 118;
 let plane2 = createGroundPlaneXZ(150, 150, 1, 1, "#FFE0B5");
-let plane = createGroundPlaneXZ(planeMaxSize, planeMaxSize);
+let plane = createGroundPlaneXZ(planeMaxSize, planeMaxSize); //Plano com os tiles
 
 scene.add(plane);
 scene.add(plane2);
@@ -46,11 +47,11 @@ scene.add(plane2);
 var floorCubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 var auxFloorCubeGeometry = new THREE.BoxGeometry(0.9, 1, 0.9);
 let materialFloorCube = setDefaultMaterial("#E6DEB3");
-let materialAuxFloorCube = setDefaultMaterial("#FEF7C6");
+let materialAuxFloorCube = setDefaultMaterial("#e8cea9");
 
-var aux = planeMaxSize / 2 - 1;
-for (let x = -aux; x <= aux; x += 1) {
-  for (let z = -aux; z <= aux; z += 1) {
+var tiles = planeMaxSize / 2 - 1;
+for (let x = -tiles; x <= tiles; x += 1) {
+  for (let z = -tiles; z <= tiles; z += 1) {
     let floorCube = new THREE.Mesh(floorCubeGeometry, materialFloorCube);
     let auxFloorCube = new THREE.Mesh(
       auxFloorCubeGeometry,
@@ -79,7 +80,7 @@ for (let i = -planeBorderWidth; i <= planeBorderWidth; i += cubeSize) {
   for (let j = -planeBorderWidth; j <= planeBorderWidth; j += cubeSize) {
     if (Math.abs(i) !== planeBorderWidth && Math.abs(j) !== planeBorderWidth)
       continue;
-    const clonedMaterial = borderCubeMaterial.clone();
+    const clonedMaterial = cubeMaterial.clone();
     const borderCube = new THREE.Mesh(cubeGeometry, clonedMaterial);
     borderCube.position.set(i, cubeSize / 2, j);
     const borderCubeBB = new THREE.Box3().setFromObject(borderCube);
@@ -88,6 +89,8 @@ for (let i = -planeBorderWidth; i <= planeBorderWidth; i += cubeSize) {
     scene.add(borderCube);
   }
 }
+
+insertCube();
 
 let camPos = new THREE.Vector3(10.5, 10.5, 10.5);
 let camUp = new THREE.Vector3(0.0, 1.0, 0.0);
@@ -342,6 +345,68 @@ function keyboardOn() {
   }
 }
 
+function insertCube() {
+
+  const positionCubes = [
+    [6.0, 0.5, 0.0],
+    [7.0, 0.5, 0.0],
+    [8.0, 0.5, 0.0],
+    [0.0, 0.5, -12.0],
+    [-8.0, 0.5, -12.0],
+    [8.0, 0.5, 12.0],
+    [22.0, 0.5, 15.0],
+    [-22.0, 0.5, -4.0],
+    [16.0, 0.5, 16.0],
+    [-16.0, 0.5, 19.0],
+    [17.0, 0.5, -19.0],
+    [0.0, 0.5, 6.0],
+    [-9.0, 0.5, 6.0],
+    [13.0, 0.5, 7.0],
+    [0.0, 0.5, 19.0],
+    [-5.0, 0.5, -9.0],
+    [15, 0.5, 10],
+    [16, 0.5, 10],
+    [17, 0.5, 10],
+    [18, 0.5, 10],
+    [19, 0.5, 10],
+    [20, 0.5, 10],
+    [21, 0.5, 10],
+    [22, 0.5, 10],
+    [23, 0.5, 10],
+    [24, 0.5, 10],
+    [25, 0.5, 10],
+    [26, 0.5, -1],
+    [26, 0.5, 0],
+    [26, 0.5, 1],
+    [26, 0.5, 2],
+    [26, 0.5, 3],
+    [26, 0.5, 4],
+    [26, 0.5, 5],
+    [26, 0.5, 6],
+    [26, 0.5, 7],
+    [26, 0.5, 8],
+    [26, 0.5, 10],
+    [26, 0.5, 9]
+
+  ];
+
+  var cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+  positionCubes.forEach(([positionX, positionY, positionZ]) => {
+
+    const clonedMaterial = cubeMaterial.clone();
+    const cube = new THREE.Mesh(cubeGeometry, clonedMaterial);
+
+    cube.position.set(positionX, positionY, positionZ);
+    const cubeBB = new THREE.Box3().setFromObject(cube);
+    collidableCubes.push(cube);
+    collidableMeshList.push(cubeBB);
+    scene.add(cube);
+
+  })
+
+
+}
+
 // Checa se a bounding box do personagem colidiu com alguma das bounding boxes dos cubos
 function checkCollision() {
   if (manBB === null || man === null) return;
@@ -375,10 +440,10 @@ function checkObjectClicked(event) {
     if (collidableCubes.includes(intersects[0].object)) {
       // Toggle cube color
       const currentObjColor = intersects[0].object.material.color;
-      if (currentObjColor.getHex() === borderCubeMaterial.color.getHex()) {
+      if (currentObjColor.getHex() === cubeMaterial.color.getHex()) {
         intersects[0].object.material.color = material.color;
       } else {
-        intersects[0].object.material.color = borderCubeMaterial.color;
+        intersects[0].object.material.color = cubeMaterial.color;
       }
     }
   }
